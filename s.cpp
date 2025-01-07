18197c55450bfde266838d2c4450a9571d289bfb
Peomarder <36908523+Peomarder@users.noreply.github.com>
2025-01-07 19:14:13 +0700

    Update s.cpp

--- s.cpp
+++ s.cpp
@@ -61,7 +61,7 @@ std::string tToJsonOrder(const std::string& tableStr) {
     result += "\n]";
     return result;
 }
-
+/*
 // Server class
 class ExchangeServer {
 private:
@@ -120,19 +120,66 @@ while(1){
     }
 };
 
-
+*/
 
 
 
 
 
 int main() {
-int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-ExchangeServer server;
+//int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+//ExchangeServer server;
 int db;
 cin>>db;
 if(db){subp("newdb");}
-server.start();  // "ctrl + c" my goat!
+
+
+        // Create user endpoint
+        server.Post("/user", [](const httplib::Request &req, httplib::Response &res) {
+            auto j = nlohmann::json::parse(req.body);
+            std::string username = j["username"];
+            string generated_key = tocharints(username); 					//idk no user key specification so idc
+            // Database operation
+            subp("INSERT INTO user VALUES ('" + ("id"+tocharints(username))+//id
+                 "', '" + username + "', '" 								//name
+				 + generated_key + "')");									//key
+            
+            res.set_content("{\n\"key\": "+generated_key+"\n}", "application/json");
+        });
+
+        // Create order endpoint
+        server.Post("/order", [](const httplib::Request &req, httplib::Response &res) {
+            auto userKey = req.get_header_value("X-USER-KEY");
+            auto j = nlohmann::json::parse(req.body);					//hi json babe
+            string order_id = generateOrderId();
+            std::string query = "INSERT INTO order VALUES ('" +
+                order_id + "', '" +
+                "id"+userKey + "', '" +
+                std::to_string(j["pair_id"].get<int>()) + "', '" + 		//idk why handle these as int and float when they become string again,
+                std::to_string(j["quantity"].get<float>()) + "', '" + 	//so like you could just check if they were numbers or idk
+                std::to_string(j["price"].get<float>()) + "', '" +
+                j["type"].get<std::string>() + "', '')";
+            
+            subp(query);
+            res.set_content("{\norder_id: "+order_id+"\n}", "application/json");
+        });
+
+        // Get orders endpoint
+        server.Get("/order", [](const httplib::Request &, httplib::Response &res) {
+            std::string result = tToJsonOrder(subp("SELECT order.order_id,order.user_id,order.lot_id,order.quantity,order.type,order.price,order.closed FROM order"));
+            res.set_content(result, "application/json");
+        });
+
+        // Get lots endpoint
+        server.Get("/lot", [](const httplib::Request &, httplib::Response &res) {
+            std::string result = subp("SELECT lot.lot_id,lot.name FROM lot");
+            res.set_content(result, "application/json");
+        });
+
+        server.listen("0.0.0.0", PORT);
+
+
+//server.start();  // "ctrl + c" my goat!
 
 return 0;
 }
